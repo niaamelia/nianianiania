0bdad059c9f75c2e188d9e83ab593bb1577d1cf1
MYP <sword.arch@yahoo.co.id>
2014-10-08 14:41:13 +0700

    commit pertama

---
+++ src/niania/Niania.java
@@ -0,0 +1,19 @@
+
+package niania;
+
+public class Niania {
+
+    public static void main(String[] args) {
+    String nama="Nia Amelia";
+System.out.println("Nama:"+nama);
+String npm="13311522";
+System.out.println("Npm:"+npm);
+String kelas="SI13E";
+System.out.println("Kelas:"+kelas);
+String jurusan="SI";
+System.out.println("Jurusan:"+jurusan);
+String ukm="hima";
+System.out.println("ukm:"+ukm);
+            
+    }
+}
